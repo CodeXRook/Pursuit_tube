3e10ab22f293bb74083600e65ce04381bf195ee2
CodeXRook <lacekitfly@gmail.com>
2019-03-03 20:58:36 -0500

    Path routes for page setup

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import { HashRouter, Route, Link } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
-      <>
-     
+
+      //NAVR BAR LINK WILL DISPLAY ON ALL PAGES
+    
+    <>  
      <HashRouter>
      <>
      <Navbar />
+     <Route path='/' exact component={Home} />
+     <Route path='/user' component={User} />
+     <Route path='/feededitor' component={FeedEditor} />
 
      </>
      </HashRouter>
