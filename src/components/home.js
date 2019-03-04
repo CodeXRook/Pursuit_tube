0c5954271699f10544c1c262e288037ddd2c68fb
CodeXRook <lacekitfly@gmail.com>
2019-03-03 21:57:38 -0500

    ESPN

--- src/components/home.js
+++ src/components/home.js
@@ -72,6 +72,7 @@ class Home extends React.Component {
 
                     <h1>Feed list</h1>
                     <div className="list-group">
+                    <a href="#" className="list-group-item list-group-item-action">ESPN First Take</a>
 
                     </div>
                     
