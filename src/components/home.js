15148af05b94adedd38b93d1b256e6b6e2a1116a
CodeXRook <lacekitfly@gmail.com>
2019-03-03 21:40:48 -0500

    lets feedCopy and url

--- src/components/home.js
+++ src/components/home.js
@@ -42,6 +42,7 @@ class Home extends React.Component {
             .then((data)=>{
                 let feedCopy = [...this.state.feedlist]
                 feedCopy[0].videos[0].title = data.items[0].snippet.title
+                feedCopy[0].videos[0].thumbnail = data.items[0].snippet.thumbnails.medium.url
             })
 
     }
