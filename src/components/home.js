f805b03d2580428ff1cb2da695b62fe72a5cee9a
CodeXRook <lacekitfly@gmail.com>
2019-03-03 21:30:31 -0500

    Props for home setup

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import axios from 'axios'; //TO MAKE API CALL
 
+class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state ={
+            feedlist:[ 
+                {
+                    query: 'ESPN FIRST TAKE',
+                    videos:[
+                        {
+                            title: 'Lebron James',
+                            thumbnail: '',
+                            channel: '',
+                            posted: '',
+                            id: '',
+                        },
 
+                    ]
+                }
+            ]
 
-
-        
+        }
+    }
+    
+}
