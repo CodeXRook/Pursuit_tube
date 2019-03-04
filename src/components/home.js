c775177bb20cd9542e8b30b9f8cc56d7498ba8a8
CodeXRook <lacekitfly@gmail.com>
2019-03-03 21:42:00 -0500

    feedCopy, again

--- src/components/home.js
+++ src/components/home.js
@@ -44,6 +44,7 @@ class Home extends React.Component {
                 feedCopy[0].videos[0].title = data.items[0].snippet.title
                 feedCopy[0].videos[0].thumbnail = data.items[0].snippet.thumbnails.medium.url
                 feedCopy[0].videos[0].channel = data.items[0].snippet.channelTitle
+                feedCopy[0].videos[0].posted = data.items[0].snippet.publishedAt
             })
 
     }
