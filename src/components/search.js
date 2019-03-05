c8891cb3f7eb0f83dc0cf1e202b633d63300f3b8
CodeXRook <lacekitfly@gmail.com>
2019-03-05 02:23:56 -0500

    const srollToBottom

--- src/components/search.js
+++ src/components/search.js
@@ -68,6 +68,7 @@ class Search extends Component {
                 const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
                 const scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
                 const clientHeight = document.documentElement.clientHeight || window.innerHeight;
+                const scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
 
             }
     }
