6a069b0b7fe29654d7190a29bb89226a807cc707
CodeXRook <lacekitfly@gmail.com>
2019-03-05 12:39:13 -0500

    feed editor complete need to fix bugs

--- src/components/feededitor.js
+++ src/components/feededitor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 class FeedEditor extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             feed: ['espn', 'drake'],
@@ -10,70 +10,73 @@ class FeedEditor extends React.Component {
             currentFeed: ''
         }
     }
-//DOES IT EXIST IN LOCAL STORAGE?, IF YES THEN SET IT TO STATE
-componentWillMount() {
-    localStorage.getItem('feed') && this.setState({
-        feed: JSON.parse(localStorage.getItem('feed')),
-        input: ''
-    })
-}
+    //DOES IT EXIST IN LOCAL STORAGE?, IF YES THEN SET IT TO STATE
+    componentWillMount() {
+        localStorage.getItem('feed') && this.setState({
+            feed: JSON.parse(localStorage.getItem('feed')),
+            input: ''
+        })
+    }
 
-clickingFeed = (e, idx) =>{
-    const currentFeed = this.state.feed[idx]
-    let array = [...this.state.feed]; 
-    let index = array.indexOf(currentFeed)
-    console.log(idx)
-    if (idx !== -1) {
-        array.splice(idx, 1);
-        this.setState({ feed: array });
-    }
-    onFeedChange =(e) =>{
-        console.log(e.target.value)
-        this.setState({ input: e.target.value })
-    }
-    addFeeder = (e) =>{
-        console.log('keystroke', e)
-        e.preventDefault();
-        if (this.state.feed.includes(this.state.input)){
-            return alert(`Feed ${this.state.input} is already in list`)
+    clickingFeed = (e, idx) => {
+        const currentFeed = this.state.feed[idx]
+        let array = [...this.state.feed];
+        let index = array.indexOf(currentFeed)
+        console.log(idx)
+        if (idx !== -1) {
+            array.splice(idx, 1);
+            this.setState({ feed: array });
         }
-         //   LET NEWUSER = THIS.STATE.USERS.PUSH(THIS.STATE.INPUT)
-         let copiedFeed = [...this.state.feed]
-         this.setState({ feed: copiedFeed })
+        onFeedChange = (e) => {
+            console.log(e.target.value)
+            this.setState({ input: e.target.value })
+        }
+        addFeeder = (e) => {
+            console.log('keystroke', e)
+            e.preventDefault();
+            if (this.state.feed.includes(this.state.input)) {
+                return alert(`Feed ${this.state.input} is already in list`)
+            }
+            //   LET NEWUSER = THIS.STATE.USERS.PUSH(THIS.STATE.INPUT)
+            let copiedFeed = [...this.state.feed]
+            this.setState({ feed: copiedFeed })
 
-    }
+        }
         //BEFORE RENDERING AND AFTER THE FUNCTION
         componentWillUpdate(nextProps, nextState)
         localStorage.setItem('feed', JSON.stringify(nextState.feed))
-  }
+    }
 
-  render() {
-      console.log(this.state)
-      return(
-          <>
-           <div className='row'>
-           
-           <div className='col-6'>
-           <div><h3>Create a New Explore Feed</h3></div>
-           <input type='text' placeholder='feed' onChange={this.onFeedChange} /><button onClick={this.addFeeder}>add</button>
-  
-           </div>
-           
-           <div className='col-6'>
-           <div><h3>Explore Feed List</h3></div>
-           <ul class="list-group"></ul>
-           {
-                this.state.feed.map((feed, i) => {
-                    let activeFeed = " ";
-                    if (feed === this.state.currentFeed) activeFeed = "active"
-                    return <li key={i} className={"list-group-item d-flex justify-content-between align-items-center"} >{feed}
-                     <span class={"badge badge-primary badge-pill " + activeFeed} onClick={(e) => this.clickingFeed(e, i)} >x</span></li>
+    render() {
+        console.log(this.state)
+        return (
+            <>
+                <div className='row'>
 
-                }
-           }
+                    <div className='col-6'>
+                        <div><h3>Create a New Explore Feed</h3></div>
+                        <input type='text' placeholder='feed' onChange={this.onFeedChange} /><button onClick={this.addFeeder}>add</button>
 
-           </div>
+                    </div>
+
+                    <div className='col-6'>
+                        <div><h3>Explore Feed List</h3></div>
+                        <ul class="list-group"></ul>
+                        {
+                            this.state.feed.map((feed, i) => {
+                                let activeFeed = " ";
+                                if (feed === this.state.currentFeed) activeFeed = "active"
+                                return <li key={i} className={"list-group-item d-flex justify-content-between align-items-center"} >{feed}
+                                    <span class={"badge badge-primary badge-pill " + activeFeed} onClick={(e) => this.clickingFeed(e, i)} >x</span></li>
+                            })
+                        }
+
+                 </ul>
+                </div>
+            </div>
           </>
       )
-  }
+    }
 }
+
+export default FeedEditor;
