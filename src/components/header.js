fde2f7366020f351096608cff462cfad2cc263cf
CodeXRook <lacekitfly@gmail.com>
2019-03-05 04:01:33 -0500

    Added ne div Navbar bugs fixed

--- src/components/header.js
+++ src/components/header.js
@@ -5,29 +5,31 @@ import './home.css';
 const Navbar = props => {
     return (
         <>
-            <div class="flex-container">
+            <div className="flex-container">
 
-                <nav class="navbar navbar-expand-lg ">
+            <div className= 'Navbar'>
+
+                <nav className="navbar navbar-expand-lg ">
 
                     <img width='20' src={require('../assets/image.jpeg')} />
 
-                    <div class="Pursuit">
+                    <div className="Pursuit">
                         <ol> <a>Pursuit Tube</a> </ol>
                     </div>
 
-                    <div class="navbar ">
+                    <div className="navbar ">
                         <Link to='/'>Home</Link>
                         <Link to='/user'>User</Link>
                         <Link to='/feededitor'>Feed Editor</Link>
                     </div>
 
-                    <input class="box" type="search" id="search" placeholder="Search" />
+                    <input className="box" type="search" id="search" placeholder="Search" />
 
                     <button className='searchButton' type='text' placeholder='Search ...' >Search</button>
 
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 
-                        <div class="navbar-nav">
+                        <div className="navbar-nav">
 
                         </div>
                     </div>
