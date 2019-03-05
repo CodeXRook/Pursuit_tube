0a22bfb4a1097a643230e8d516af0256b5d4af42
CodeXRook <lacekitfly@gmail.com>
2019-03-05 04:22:26 -0500

    componenWill mount

--- src/components/feededitor.js
+++ src/components/feededitor.js
@@ -5,7 +5,12 @@ class FeedEditor extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            
+            feed: ['espn', 'drake'],
+            input: '',
+            currentFeed: ''
         }
     }
+//DOES IT EXIST IN LOCAL STORAGE?, IF YES THEN SET IT TO STATE
+componentWillMount() {
+
 }
