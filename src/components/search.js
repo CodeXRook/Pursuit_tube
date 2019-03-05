aaae36399d383b11b51042a2cd131a47ea9f8811
CodeXRook <lacekitfly@gmail.com>
2019-03-05 02:23:10 -0500

    const srollHeigt inner window

--- src/components/search.js
+++ src/components/search.js
@@ -67,6 +67,7 @@ class Search extends Component {
             handleOnScroll =()=> {
                 const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
                 const scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+                const clientHeight = document.documentElement.clientHeight || window.innerHeight;
 
             }
     }
