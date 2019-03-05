816cae59fd3fd022b01aa883c715965c6e2b31aa
CodeXRook <lacekitfly@gmail.com>
2019-03-05 00:14:46 -0500

    videoPlayer props though no set state

--- src/components/video.js
+++ src/components/video.js
@@ -2,5 +2,10 @@ import React from 'react';
 
 //NOT SURE IF THE CAPITAL ON VIDEO IS CORRECT
 const VideoPlayer =(props) =>{
+const {id, title, channel, posted, thumbnail} = props.Video
+const link =`https://www.youtube.com/embed/${id}?autoplay=1&origin=http://localhost:3000`;
+
+return(
     
+)
 }
