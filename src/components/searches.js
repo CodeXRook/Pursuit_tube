7965932ccee514a0d7a952077635f4865ef687b4
CodeXRook <lacekitfly@gmail.com>
2019-03-05 00:25:14 -0500

    Search state

--- src/components/searches.js
+++ src/components/searches.js
@@ -2,7 +2,21 @@ import React from 'react';
 import VideoPlayer from './video';
 import axios from 'axios';
 import './home.css';
-import moment from 'moment';
+import moment from 'moment'; //NEED TO GO OVER MOMENT;
+
+class Search extend React.Component {
+construcor(props){
+    super(props)
+    this.state ={
+        input: 'Bethel',
+        videos: [],
+    }
+ }
+
+    loadVideos = (query) => {
+        
+    }
+}
 
 
 export default Search;
