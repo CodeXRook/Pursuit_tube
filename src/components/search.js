27cdfb7255004e97e1a10fca13d992c2a25bb670
CodeXRook <lacekitfly@gmail.com>
2019-03-05 02:54:36 -0500

    retun key in row

--- src/components/search.js
+++ src/components/search.js
@@ -81,6 +81,7 @@ class Search extends Component {
                   <div className="jumbotron">
                   <div style={{marginLeft:'10%'}}>
                   {this.state.data.map((vid, i)=>{
+                      return <div className='hover row' key={i}>
                   </div>
                   </div>
 
