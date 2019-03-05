12709530bc09cc9c795358276fb33f60c6c49271
CodeXRook <lacekitfly@gmail.com>
2019-03-05 02:51:54 -0500

    class span loading

--- src/components/search.js
+++ src/components/search.js
@@ -74,7 +74,7 @@ class Search extends Component {
 
         render(){
             if(this.state.isLoading) return <div className="spinner-border text-info" role="status">
-            
+             <span className="sr-only">Loading...</span>
         }
     }
 }
